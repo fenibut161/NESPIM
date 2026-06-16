5a96ae4e24076531cc2a3b639825c9fe2901e929
fenibut161 <fenibut001@gmail.com>
2026-06-16 19:59:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ GIGACHAT_AUTH_KEY = os.getenv("GIGACHAT_AUTH_KEY")   # для генерации
 
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN, threaded=False, num_workers=1)
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
 bot.request_timeout = 120
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
